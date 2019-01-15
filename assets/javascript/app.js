e1000f4016e3837257f914fc62822ef4a41dad51
Jennifer M <jennifermartinez@Jennifers-MacBook-Pro.local>
2019-01-15 13:44:53 -0800

    Trivia

---
+++ assets/javascript/app.js
@@ -0,0 +1,29 @@
+// Create global variables for the game //
+
+
+// Document ready //
+
+$(document).ready(function() {
+
+// START BUTTON click takes user to new screen //
+
+
+
+// New screen STARTS the TIMER, counting down (120 sec.) //
+
+// Only ONE available choice can be selected from the answers to each question //
+
+// CORRECT answer are added to the counter displayed at the end of the game //
+
+// INCORRECT answer are added to the counter displayed at the end of the game //
+
+// UNANSWERED question(s) are added to the counter displayed at the end of the game //
+
+// If DONE BUTTON is clicked, it displays a new screen //
+
+// The new screen displays, GAME OVER and the users correct/incorrect/unanswered tally //
+
+// If the timer runs out before the user clicks the DONE BUTTON, a new screen
+// will display, GAME OVER and the users correct/incorrect/unanswered tally //
+
+});
